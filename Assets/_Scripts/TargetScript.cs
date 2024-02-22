d56e1b63a7eb903584284fc1aec612323efcfd47
robert-1607 <64499850+robert-777@users.noreply.github.com>
2024-02-22 13:43:16 +0100

    Update ShootingRange Scene
    Update ShootingRange Scene to implement basic functionality (funger gun, targets, environment).

---
+++ Assets/_Scripts/TargetScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    private bool isUp = true;
+
+    [SerializeField] private float tweenTime = 0.4f;
+
+    [Tooltip("Amount of seconds a target stays down before swinging back up 'reanimating' itself. If '<=0', the target will permanently stay down.")]
+    [SerializeField] private int downtime = 5;
+
+    // Catch Collision and make target fall back.
+    private void OnCollisionEnter(Collision collision)
+    {
+        // only execute when target actually up
+        if (isUp)
+        {
+            isUp = false;
+
+            // tween the target into down position
+            LeanTween.cancelAll();
+            LeanTween.rotate(gameObject, new Vector3(90, 0, 0), tweenTime).setEase(LeanTweenType.easeOutBounce);
+
+            if (downtime > 0) StartCoroutine(ReactivateTarget());
+        }
+    }
+
+    private IEnumerator ReactivateTarget()
+    {
+        yield return new WaitForSeconds(downtime);
+
+        // tweeen target up
+        LeanTween.cancelAll();
+        LeanTween.rotate(gameObject, Vector3.zero, tweenTime).setEase(LeanTweenType.easeOutBounce);
+
+        isUp = true;
+    }
+}
