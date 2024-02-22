da645a8c802efa56ec884c57f29fd1bbe196e971
robert-1607 <64499850+robert-777@users.noreply.github.com>
2024-02-22 11:36:54 +0100

    Create ShootingRange Scene
    Basic scene setup and first finger gun mechanic.

---
+++ Assets/_Scripts/BulletShooter.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShooter : MonoBehaviour
+{
+    [Tooltip("Bullet Prefab which will be shot when Finge Gun Pose Ends.")]
+    [SerializeField] private GameObject bulletPrefab;
+
+    [Tooltip("Force value that determins how fast the bullet will be shot.")]
+    [SerializeField] private float shotStrength = 100f;
+
+    [Tooltip("Transform that holds the bullets until they are destroyed.")]
+    [SerializeField] private Transform bulletHolder;
+
+    // Public Method to shoot Bullet prefab when called.
+    public void ShootBullet()
+    {
+        Rigidbody bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity, bulletHolder).GetComponent<Rigidbody>();
+        bullet.AddForce(transform.forward * shotStrength);
+        Destroy(bullet.gameObject, 10f);
+    }
+
+}
